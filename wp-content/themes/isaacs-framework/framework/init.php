6be9fd3c0ffe7473efd8c7bb6edd5f0258d56930
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2015-03-11 19:05:26 +0000

    setting up the index.php page

---
+++ wp-content/themes/isaacs-framework/framework/init.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isaac
+ * Date: 11/03/15
+ * Time: 18:34
+ */ 
--- wp-content/themes/isaacs-framework/index.php
+++ wp-content/themes/isaacs-framework/index.php
@@ -4,4 +4,18 @@
  * User: isaac
  * Date: 11/03/15
  * Time: 13:03
- */ 
+ *
+ * index.php
+ */
+
+?>
+
+<div class="main-content col-md-8" role="main">
+		<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+</div> <!-- end main-content -->
